b046e58eb38866988ab93726934c544b2b807be6
Michal Schwarz <michal.schwarz@golemos.com>
2023-09-20 10:04:12 +0200

    Refactoring: try to enable package system

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sim800l',
+      version='1.0',
+      description='SIM 800l',
+      author='https://github.com/jakhax',
+      author_email='jackogina60@gmail.com',
+      url='https://github.com/MichalSchwarz/raspberry-pi-sim800l-gsm-module',
+      packages=['sim800l'],
+     )
