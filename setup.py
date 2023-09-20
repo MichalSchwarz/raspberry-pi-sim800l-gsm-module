5b3ac2c609dad537160638928e061410cb9356aa
Michal Schwarz <michal.schwarz@golemos.com>
2023-09-20 12:44:00 +0200

    Refactoring: try to enable package system

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(name='Sim800l',
-      version='1.0.4',
+      version='1.0.5',
       description='SIM 800l',
       author='https://github.com/jakhax',
       author_email='jackogina60@gmail.com',
       url='https://github.com/MichalSchwarz/raspberry-pi-sim800l-gsm-module',
       packages=find_packages(exclude=['tests', 'tests.*']),
       install_requires=[
-          'serial',
+          'pyserial',
       ],
      )
